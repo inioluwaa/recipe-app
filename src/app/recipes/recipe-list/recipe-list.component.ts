88574d6292bf49c8e7a091dd99988bee2943eec0
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2017-09-27 12:28:06 +0100

    Add test ingredients

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,9 +10,9 @@ import { Recipe } from "../recipe.model";
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 
-    'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg'),
+    'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
     new Recipe('A Test Recipe', 'This is simply a test', 
-    'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg')
+    'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
   ];
     
 
