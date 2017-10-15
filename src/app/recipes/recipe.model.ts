1db2a49640533b5c91f98a804cc392a5a0421683
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2017-10-15 07:01:29 +0100

    add ingredients to recipes

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,15 @@
+import {Ingredient} from '../shared/ingredient.model';
+
 export class Recipe {
   public name: string;
   public description: string;
   public imagePath: string;
+  public ingredient: Ingredient[];
 
-  constructor(name: string, desc: string, imagePath: string){
+  constructor(name: string, desc: string, imagePath: string, ingredient: Ingredient[]) {
       this.name = name;
       this.description = desc;
       this.imagePath = imagePath;
+      this.ingredient = ingredient;
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,24 @@
 import {EventEmitter} from '@angular/core';
 
 import {Recipe} from './recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test',
-      'https://rms.knorrwhatsfordinner.co.za/images/defaults/recipe.jpg'),
+    new Recipe('Tasty Schnitzel', 'A super tasty schnitzel - just awesome!',
+      'https://rms.knorrwhatsfordinner.co.za/images/defaults/recipe.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
     new Recipe('Another Test Recipe', 'This is simply a test',
-      'https://rms.knorrwhatsfordinner.co.za/images/defaults/recipe.jpg')
+      'https://rms.knorrwhatsfordinner.co.za/images/defaults/recipe.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
   ];
   getRecipes() {
     return this.recipes.slice();
